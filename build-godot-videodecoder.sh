6a0f54f967d29061b89092ed3e6060548bff3932
Silphendio <markus.ginthoer@gmx.at>
2024-02-13 09:56:36 +0100

    removed a bunch of unnecessary files

--- build-godot-videodecoder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd thirdparty/godot-videodecoder/
-gcc src/gdnative_videodecoder.c -I godot_include/ -I ffmpeg-static/ -lavcodec -lswresample -lavutil -lavformat -lswscale -fPIC -shared -o libgdnative_videodecoder.so
-mkdir -p ../../addons/bin/x11
-mv libgdnative_videodecoder.so ../../addons/bin/x11
-cp videodecoder.gdnlib ../../addons/
-cd ../..
