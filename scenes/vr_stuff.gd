51424d9f6b4eac6f82b6e1b88f67bc87cd9b56f5
Silphendio <markus.ginthoer@gmx.at>
2024-08-22 18:41:44 +0200

    make contollers invisible when turned off

--- scenes/vr_stuff.gd
+++ scenes/vr_stuff.gd
@@ -8,3 +8,11 @@ extends Node3D
 @onready var left_hand_mesh = $XROrigin3D/LeftHand/MeshInstance3D
 @onready var right_hand_mesh = $XROrigin3D/RightHand/MeshInstance3D
 
+
+
+func _on_left_hand_tracking_changed(tracking):
+	left_hand.visible = tracking
+
+
+func _on_right_hand_tracking_changed(tracking):
+	right_hand.visible = tracking
